4e1c15065bb56394f37d47c840f9f9ecaca53861
Souvik Goon <souvikgoon22@gmail.com>
2025-03-30 03:28:48 +0530

    disable static file serving from backend

--- server.js
+++ server.js
@@ -23,16 +23,20 @@ app.use("/api/user", authRoute);
 app.use("/api/transactions", transactionsRoute);
 
 //if running in production, serve index page
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("Api Running.");
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("Api Running.");
+//   });
+// }
+
+app.get("/", (req, res) => {
+  res.send("Api Running.");
+});
 
 //start listening
 const port = process.env.PORT || 5000;
